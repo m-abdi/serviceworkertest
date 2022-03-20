a6c3f2cc91c273763b05d43051ae3608730e836c
Mehdi <m.abdi.public@gmail.com>
2022-03-20 15:01:30 +0330

    path changed

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ window.addEventListener('load', async () => {
   if ('serviceWorker' in navigator) {
     try {
       const registration = await navigator.serviceWorker.register('/serviceworkertest/sw1.js', {
+        scope: "/serviceworkertest/"
       });
       if (registration.installing) {
         console.log('Service worker installing');
